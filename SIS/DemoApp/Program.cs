e52c178de11276356e36b81f59604166d8c20018
Petko Pedev <petko.pedev@gmail.com>
2020-01-27 18:53:44 +0200

    Adding SIS project to the repo.

---
+++ SIS/DemoApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using SIS.HTTP;
+using System;
+using System.Threading.Tasks;
+
+namespace DemoApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Actions:
+            // / => response IndexPage(request)
+            // GET /Contact => response ShowContactForm(request)
+            // POST /Contact => response FillContactForm(request)
+
+            var httpServer = new HttpServer(80);
+            await httpServer.StartAsync();
+        }
+    }
+}
