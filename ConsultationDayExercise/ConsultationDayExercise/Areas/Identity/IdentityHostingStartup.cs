977a7f84bf92dfad6d2fb42b4205f3f32f04eb51
Petko Pedev <petko.pedev@gmail.com>
2020-03-05 23:52:04 +0200

    Adding Consultation day exercise with areas and roles.

---
+++ ConsultationDayExercise/ConsultationDayExercise/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ConsultationDayExercise.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ConsultationDayExercise.Areas.Identity.IdentityHostingStartup))]
+namespace ConsultationDayExercise.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ ConsultationDayExercise/ConsultationDayExercise/SeedData/ApplicationDbContextSeeder.cs
@@ -0,0 +1,88 @@
+﻿using ConsultationDayExercise.Data;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace ConsultationDayExercise.SeedData
+{
+    public class ApplicationDbContextSeeder
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly ApplicationDbContext dbContext;
+
+        public ApplicationDbContextSeeder(IServiceProvider serviceProvider, ApplicationDbContext dbContext)
+        {
+            this.userManager = serviceProvider.GetService<UserManager<IdentityUser>>();
+            this.roleManager = serviceProvider.GetService<RoleManager<IdentityRole>>();
+            this.dbContext = dbContext;
+        }
+        public async Task SeedDataAsync()
+        {
+            await SeedUsersAsync();
+            await SeedRolesAsync();
+            await SeedUserToRolesAsync();
+        }
+
+        private async Task SeedUserToRolesAsync()
+        {
+            var user = await userManager.FindByNameAsync("Peter");
+            var role = await roleManager.FindByNameAsync("Admin");
+
+            var exists = dbContext.UserRoles.Any(x => x.UserId == user.Id && x.RoleId == role.Id);
+            
+            if (exists)
+            {
+                return;
+            }
+
+            dbContext.UserRoles.Add(new IdentityUserRole<string>
+            {
+                RoleId = role.Id,
+                UserId = user.Id
+            });
+
+            await dbContext.SaveChangesAsync();
+        }
+
+        private async Task SeedRolesAsync()
+        {
+            var role = await roleManager.FindByNameAsync("Admin");
+            
+            if (role != null)
+            {
+                return;
+            }
+
+            await roleManager.CreateAsync(new IdentityRole
+            {
+                Name = "Admin"
+            });
+        }
+
+        private async Task SeedUsersAsync()
+        {
+            var user = await userManager.FindByNameAsync("Peter");
+            if (user != null)
+            {
+                return;
+            }
+
+            var result = await userManager.CreateAsync(new IdentityUser
+            {
+                UserName = "Peter",
+                Email = "Peter@abv.bg",
+                EmailConfirmed = true
+
+            },
+            "asdasd");
+
+            //if (!result.Succeeded)
+            //{
+            //
+            //}
+        }
+    }
+}
