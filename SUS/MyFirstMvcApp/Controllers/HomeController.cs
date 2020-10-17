a603f461c1ee2838a211c2df9ecc3ff34d590436
Petko Pedev <petko.pedev@gmail.com>
2020-10-18 00:09:07 +0300

    Yet another commit of C# Web Basic Course.

---
+++ SUS/MyFirstMvcApp/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using SUS.HTTP;
+using SUS.MvcFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyFirstMvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HttpResponse Index(HttpRequest request)
+        {
+            return this.View();
+        }
+    }
+}
---
+++ SUS/MyFirstMvcApp/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using SUS.HTTP;
+using SUS.MvcFramework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MyFirstMvcApp.Controllers
+{
+    public class UsersController : Controller
+    {
+        public HttpResponse Login(HttpRequest request)
+        {
+            return this.View();
+        }
+
+        public HttpResponse Register(HttpRequest request)
+        {
+            return this.View();
+        }
+    }
+}
