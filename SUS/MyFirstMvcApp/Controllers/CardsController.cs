3fa80d0d84405b82b90c5f30e7aff5cf59fc19bb
Petko Pedev <petko.pedev@gmail.com>
2020-11-06 17:14:36 +0200

    Improve model binding.

--- SUS/MyFirstMvcApp/Controllers/CardsController.cs
+++ SUS/MyFirstMvcApp/Controllers/CardsController.cs
@@ -1,5 +1,5 @@
 ﻿using BattleCards.Data;
-using BattleCards.ViewModels;
+using BattleCards.ViewModels.Cards;
 using SUS.HTTP;
 using SUS.MvcFramework;
 using System;
@@ -27,7 +27,7 @@ namespace BattleCards.Controllers
         }
 
         [HttpPost("/Cards/Add")]
-        public HttpResponse DoAdd(string attack, string health, string description, string name, string image, string keyword)
+        public HttpResponse DoAdd(AddCardInputModel model)
         {
             if (!this.IsUserSignedIn())
             {
@@ -43,12 +43,12 @@ namespace BattleCards.Controllers
 
             this.db.Cards.Add(new Card
             {
-                Attack = int.Parse(attack),
-                Health = int.Parse(health),
-                Description = description,
-                Name = name,
-                ImageUrl = image,
-                Keyword = keyword,
+                Attack = model.Attack,
+                Health = model.Health,
+                Description = model.Description,
+                Name = model.Name,
+                ImageUrl = model.Image,
+                Keyword = model.Keyword,
             });
             this.db.SaveChanges();
             
