2abe6744bb75529c3cb30ca89eedeaa27d02df2b
Petko Pedev <petko.pedev@gmail.com>
2020-11-10 12:19:19 +0200

    Adding Shared Trip Project to the repo.

---
+++ SUS/SharedTrip/Services/ITripsService.cs
@@ -0,0 +1,12 @@
+﻿using SharedTrip.ViewModels.Trips;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedTrip.Services
+{
+    public interface ITripsService
+    {
+        void Create(AddTripInputModel trip);
+    }
+}
---
+++ SUS/SharedTrip/Services/TripsService.cs
@@ -0,0 +1,34 @@
+﻿using SharedTrip.Models;
+using SharedTrip.ViewModels.Trips;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedTrip.Services
+{
+    public class TripsService : ITripsService
+    {
+        private readonly ApplicationDbContext db;
+
+        public TripsService(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        public void Create(AddTripInputModel trip)
+        {
+            var dbTrip = new Trip
+            {
+                StartPoint = trip.StartPoint,
+                EndPoint = trip.EndPoint,
+                DepartureTime = trip.DepartureTime,
+                ImagePath = trip.ImagePath,
+                Seats = trip.Seats,
+                Description = trip.Description,
+
+            };
+
+            this.db.Add(dbTrip);
+            this.db.SaveChanges();
+        }
+    }
+}
