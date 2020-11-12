876bf25aa686f682312a90568f8c5047619df5a1
Petko Pedev <petko.pedev@gmail.com>
2020-11-12 11:41:24 +0200

    Add MUSACA
    Adding MUSACA to the Apps. Initial commit.

---
+++ SUS/MUSACA/Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MUSACA.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions db)
+            :base(db)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(DatabaseConfiguration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
---
+++ SUS/MUSACA/Data/DatabaseConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace MUSACA.Data
+{
+    public class DatabaseConfiguration
+    {
+        public const string ConnectionString =
+            @"Server=DESKTOP-E5PBLPH\SQLEXPRESS;Database=MUSACA;Trusted_Connection=True;Integrated Security=True;";
+    }
+}
